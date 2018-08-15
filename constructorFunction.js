c3b6670905e9584a06180135ff2cb3b3d5184860
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-16 00:33:54 +0100

    Add constructor function
    * Reviewing name space

--- constructorFunction.js
+++ constructorFunction.js
@@ -1,3 +1,5 @@
+// Manually create an object - not very clever...
+
 function createNewPerson(name) {
   var obj = {};
   obj.name = name;
@@ -6,3 +8,14 @@ function createNewPerson(name) {
   };
   return obj;
 };
+
+// Construtor function
+function Person(name) {
+  this.name = name;
+  this.greeting = function() {
+    alert('Hi I\'m ' + this.name + '.');
+  };
+};
+
+var person1 = new Person('Jeremy');
+var person2 = new Person('Myra');
