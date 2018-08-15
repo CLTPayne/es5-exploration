23d37d2c140660594238ecc21df16a015a1f8ac8
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-15 23:28:08 +0100

    Set up index and object literal
    * Explore in the chrome terminal
    * Process recap

---
+++ objectLiteral.js
@@ -0,0 +1,14 @@
+var person = {
+  name: ['Bob', 'Smith'],
+  age: 32,
+  gender: 'male',
+  interests: ['coding', 'jogging'],
+  bio: function() {
+    alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age +
+      ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1]
+      + '.');
+  },
+  greeting: function() {
+    alert('Hi! I\'m ' + this.name[0] + '.');
+  }
+};
