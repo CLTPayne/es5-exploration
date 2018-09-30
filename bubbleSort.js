a94e510f25d0a5cf050b53df5cbec1a8ca83e729
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-30 21:23:20 +0100

    Add reverse bubble sort logic

--- bubbleSort.js
+++ bubbleSort.js
@@ -11,6 +11,19 @@ function bubbleSort(array) {
   return array;
 }
 
-console.log(bubbleSort([1,2,3,4]));
+function bubbleReverseSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let ii = 0; ii < array.length; ii++) {
+      if (array[ii] < array[ii + 1]) {
+        let temp = array[ii];
+        array[ii] = array[ii + 1];
+        array[ii + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
 
 console.log(bubbleSort([4,2,1,3,7,5,6]));
+
+console.log(bubbleReverseSort([1,2,3,4]));
