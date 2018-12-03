5da8887c946e61e20f528be5d746119befd33c35
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-12-03 23:06:57 +0000

    Why name your function expressions
    * 3 reasons from Kyle Simpson

---
+++ namedFunctionExpression.js
@@ -0,0 +1,11 @@
+// A named function expression is preferable to an anonymous one because:
+
+// 1. Povides a way to refer to the function from inside of itself
+//      e.g. for recursion
+//    The name will also be readonly
+//    Be consistent
+
+// 2. Having a named function expression will show in your stack trace.
+//    So easier debugging
+
+// 3. More self documenting code. Useful names tell other devs what your code does
